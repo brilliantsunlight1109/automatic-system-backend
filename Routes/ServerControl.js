1cd180d5217a90a31b1d33930ba2d89df53e97f1
brilliantsunlight <brilliantsunlight1109@gmail.com>
2024-04-08 16:22:21 +0900

    added sync_time

--- Routes/ServerControl.js
+++ Routes/ServerControl.js
@@ -1,18 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
-const {
-  getServer,
-  StyleServerStart,
-  StyleServerStop,
-  BlogServerStart,
-  BlogServerStop,
-} = require("../Controllers/ServerControl");
+// const {
+//   getServer,
+//   StyleServerStart,
+//   StyleServerStop,
+//   BlogServerStart,
+//   BlogServerStop,
+// } = require("../Controllers/ServerControl");
 
-router.get("/", getServer);
-router.put("/styleserverstart", StyleServerStart);
-router.put("/styleserverstop", StyleServerStop);
-router.put("/blogserverstart", BlogServerStart);
-router.put("/blogserverstop", BlogServerStop);
+// router.get("/", getServer);
+// router.put("/styleserverstart", StyleServerStart);
+// router.put("/styleserverstop", StyleServerStop);
+// router.put("/blogserverstart", BlogServerStart);
+// router.put("/blogserverstop", BlogServerStop);
+
+const {
+  upsertServerControl,
+  getServerControl,
+} = require("../Controllers/ServerControl");
+router.post("/delete_time", upsertServerControl);
+router.get("/:style_tokyo_id", getServerControl);
 
 module.exports = router;
